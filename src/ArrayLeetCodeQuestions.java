36b1849a8b6f97de238b387c0c1bf9eed9a777ef
Harish.R <harish2000007@gmail.com>
2024-08-14 11:54:19 +0530

    Example Problems

---
+++ src/ArrayLeetCodeQuestions.java
@@ -0,0 +1,94 @@
+
+//NO IDEA-----------------------------------------------------------------------------------------------------------------------
+// How Many Numbers Are Smaller Than the Current Number 5ms
+//Check if the Sentence Is Pangram -10--Completed
+//Find the Highest Altitude--Completed
+//Matrix Diagonal Sum  ------not completed --Completed
+//Cells with Odd Values in a Matrix------not completed--COmpleted
+//Add to Array-Form of Integer------not completed
+//Determine Whether Matrix Can Be Obtained By Rotation
+//Minimum Cost to Move Chips to The Same Position --completed
+//Matrix Diagonal Sum --need to complet after binary search --Completed
+//Maximum Population Year //want to see in youtube
+
+//27,22,20,19,15
+
+
+// DIFFICULT USE DIFFERNT CONCEPT ----------------------------------------------------------------------------------
+
+
+//Shuffle the Array //Completed
+//Kids With the Greatest Number of Candies //Completed
+//Number of Good Pairs see tha best  //Completed
+// How Many Numbers Are Smaller Than the Current Number 5ms --complete in night 26-6
+//Count Items Matching a Rule  --complete in night 25 -6 //Completed
+//Matrix Diagonal Sum  ------not completed  =---more difficult to understand --Completed
+//Cells with Odd Values in a Matrix------not completed =---more difficult to understand
+//Add to Array-Form of Integer------not completed =---more difficult to understand
+//Determine Whether Matrix Can Be Obtained By Rotation=---more difficult to understand
+//Find N Unique Integers Sum up to Zero many methods are there want to do ---completed
+
+
+public class ArrayLeetCodeQuestions {
+
+//    Easy----------------------------------------------------------------------------------------------------
+
+//    Build Array from Permutation
+    /*
+    Input: nums = [0,2,1,5,3,4]
+    Output: [0,1,2,4,5,3]
+    Normal For Each using new array=nums[nums[i]];
+    * */
+//    Concatenation of Array
+//    Running Sum of 1d Array
+//    Richest Customer Wealth
+//    Shuffle the Array
+//    Kids With the Greatest Number of Candies
+//    Number of Good Pairs
+//    How Many Numbers Are Smaller Than the Current Number
+
+//    Create Target Array in the Given Order
+//        Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+//        Output: [0,4,1,3,2]
+         // Using ArrayList we can loop like add(index[i],nums[i]) and add the elements and convert to arr
+
+//    Check if the Sentence Is Pangram
+//    Count Items Matching a Rule
+       //Best way use recursion instead of for loop
+//    Find the Highest Altitude
+//    Flipping an Image
+//    Cells with Odd Values in a Matrix
+//    Matrix Diagonal Sum
+//    Find Numbers with Even Number of Digits
+//    Transpose Matrix
+//    Add to Array-Form of Integer
+//    Maximum Population Year
+//    Determine Whether Matrix Can Be Obtained By Rotation
+//    Two Sum
+//    Find N Unique Integers Sum up to Zero
+//    Lucky Number In a Matrix
+//    Maximum Subarray
+//    Reshape the Matrix
+//    Plus One
+//    Remove Duplicates from Sorted Array
+//    Minimum Cost to Move Chips to The Same Position
+
+//    Medium ----------------------------------------------------------------------------------------------------
+
+//    Spiral Matrix
+//    Spiral Matrix II
+//    Spiral Matrix III
+//    Set Matrix Zeroes
+//    Product of Array Except Self
+//    Find First and Last Position of Element in Sorted Array
+//    Jump Game
+//    Rotate Array
+//    Sort Colors
+//    House Robber
+//
+//    Hard--------------------------------------------------------------------------------------------------------
+
+//    Max Value of Equation
+//    First Missing Positive
+//    Good Array
+}
